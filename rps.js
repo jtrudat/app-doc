228ecab0db9cd923586177234c44f30c67a6c605
Erik Tatro <eriktatro@gmail.com>
2022-06-11 12:43:03 -0700

    minor js addition

--- rps.js
+++ rps.js
@@ -6,3 +6,16 @@
 //display image for winning and losing
 //need to determine computer selection
 //need to compare computer selection against user selection to determine winner
+
+let userChoice
+let computerChoice
+let result
+let percentWon
+let timesWon = 0
+let timesPlayed = 0
+let computerChoiceDisplay = document.getElementById('computer-choice')
+let userChoiceDisplay = document.getElementById('user-choice')
+let resultDisplay = document.getElementById('result')
+let possibleChoices = document.querySelectorAll('button')
+let timesPlayedDisplay = document.getElementById('rounds')
+let percentWonDisplay = document.getElementById('percentStat')
