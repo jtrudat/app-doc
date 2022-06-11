927fc016717055351ea06586946d59adbec7032f
Erik Tatro <eriktatro@gmail.com>
2022-06-11 12:27:48 -0700

    html cleanup and pseduo added

---
+++ rps.js
@@ -0,0 +1,8 @@
+//display a basic title of the game being played
+//display the selection's of both the user and the computer
+//display three buttons with both visual and written depiction of the user's selection of paper rock scissors
+//display a button for reset
+//display statistics for number of times played and winning percentages
+//display image for winning and losing
+//need to determine computer selection
+//need to compare computer selection against user selection to determine winner
