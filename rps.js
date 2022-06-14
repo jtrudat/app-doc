d812a23f966445c3b5dcdf4f6698cd3c08959be6
Erik Tatro <eriktatro@gmail.com>
2022-06-13 21:44:04 -0700

    img ids and computerChoice

--- rps.js
+++ rps.js
@@ -6,7 +6,7 @@
 //display image for winning and losing
 //need to determine computer selection
 //need to compare computer selection against user selection to determine winner
-
+let compCheck
 let myChoice
 let computerChoice
 let result
@@ -20,11 +20,46 @@ let buttonPicks = document.querySelectorAll('button')
 let timesPlayedDisplay = document.getElementById('rounds')
 let percentWonDisplay = document.getElementById('percentStat')
 
-buttonPicks.forEach((targetButton)=>{
-        targetButton.addEventListener('click', (targetHit)=>{
-        myChoice = targetHit.target.id
+buttonPicks.forEach((myButton)=>{
+        myButton.addEventListener('click', (myPick)=>{
+        myChoice = myPick.target.id
         console.log(myChoice)
-        myChoiceDisplay.innerHTML = `Your Choice : ${myChoice}`
-
+        myChoiceDisplay.innerHTML = `You drew : ${myChoice}`
+        myChoiceIcon()
+        compGenerator()
+        console.log(compCheck)
     })
 })
+function myChoiceIcon (){
+    if(myChoice === 'rock'){
+        let rockImg = document.getElementById('userImg')
+        rockImg.src = "./assets/rock.png"
+        rockImg.alt = "a rock"
+    }
+    else if(myChoice === 'paper'){
+        let paperImg = document.getElementById('userImg')
+        paperImg.src = "./assets/paper.png"
+        paperImg.alt = "a roll of toilet paper"
+    }
+    else if(myChoice === 'scissors'){
+        let scissorsImg = document.getElementById('userImg')
+        scissorsImg.src = "./assets/scissors.png"
+        scissorsImg.alt = "scissors with red handle"
+    }
+}
+function compGenerator(){
+    let randomNumber = Math.floor(Math.random()*3) + 1
+    compCheck = randomNumber
+    if(randomNumber === 1){
+        computerChoice = 'rock'
+        let rockImg = document.getElementById('compImg')
+        rockImg.src = "./assets/rock.png" 
+    }
+    //else if(){
+
+    //}
+   // else if(){
+
+   // }
+    computerChoiceDisplay.innerHTML = `Computer drew: ${computerChoice}`
+}  
