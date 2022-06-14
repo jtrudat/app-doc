08f99b17d24c6f5c95762a6a30a4c04b7b0651dc
Erik Tatro <eriktatro@gmail.com>
2022-06-14 02:20:48 -0700

    winning images

--- rps.js
+++ rps.js
@@ -9,8 +9,8 @@
 let compCheck
 let myChoice
 let computerChoice
-let result
-let percentWon
+let winningPecentage
+let winners
 let timesWon = 0
 let timesPlayed = 0
 let computerChoiceDisplay = document.getElementById('computer-choice')
@@ -20,6 +20,7 @@ let buttonPicks = document.querySelectorAll('button')
 let timesPlayedDisplay = document.getElementById('rounds')
 let percentWonDisplay = document.getElementById('percentStat')
 
+
 buttonPicks.forEach((myButton)=>{
         myButton.addEventListener('click', (myPick)=>{
         myChoice = myPick.target.id
@@ -27,8 +28,10 @@ buttonPicks.forEach((myButton)=>{
         myChoiceDisplay.innerHTML = `You drew : ${myChoice}`
         myChoiceIcon()
         compGenerator()
-        console.log(compCheck)
-    })
+        result()
+        percentWon()
+        Trophy()
+   })
 })
 function myChoiceIcon (){
     if(myChoice === 'rock'){
@@ -46,6 +49,8 @@ function myChoiceIcon (){
         scissorsImg.src = "./assets/scissors.png"
         scissorsImg.alt = "scissors with red handle"
     }
+    timesPlayed += 1
+    timesPlayedDisplay.innerHTML = `number of times played this session: ${timesPlayed}`
 }
 function compGenerator(){
     let randomNumber = Math.floor(Math.random()*3) + 1
@@ -53,13 +58,82 @@ function compGenerator(){
     if(randomNumber === 1){
         computerChoice = 'rock'
         let rockImg = document.getElementById('compImg')
-        rockImg.src = "./assets/rock.png" 
+        rockImg.src = "./assets/rock.png"
+        rockImg.alt = 'gray rock' 
     }
-    //else if(){
-
-    //}
-   // else if(){
-
-   // }
+    else if(randomNumber === 2){
+        computerChoice = 'paper'
+        let paperImg = document.getElementById('compImg')
+        paperImg.src = "./assets/paper.png"
+        paperImg.alt = 'roll of toilet paper'
+    }
+   else if(randomNumber === 3){
+        computerChoice = 'scissors'
+        let scissorsImg = document.getElementById('compImg')
+        scissorsImg.src = "./assets/scissors.png"
+        scissorsImg.alt = "red handle scissors"
+    }
     computerChoiceDisplay.innerHTML = `Computer drew: ${computerChoice}`
+    console.log(compCheck, computerChoice)
 }  
+function result(){
+    if(computerChoice === myChoice){
+           winners = 'draw'
+    }
+    else if(computerChoice === 'rock' && myChoice ==='scissors'){
+        winners = 'Sorry but you lose'
+    }
+    else if(computerChoice === 'paper' && myChoice === 'rock'){
+        winners = 'Sorry but you lose'
+    }
+    else if(computerChoice === 'scissors' && myChoice === 'paper'){
+        winners = 'Sorry but you lose'
+    }
+    else if(computerChoice === 'rock' && myChoice === 'paper'){
+        timesWon += 1
+        winners = 'You win with paper covering rock'
+    }
+    else if(computerChoice === 'paper' && myChoice === 'scissors'){
+        timesWon += 1
+        winners = 'You win with scissors cutting paper'
+    }
+    else if(computerChoice === 'scissors' && myChoice === 'rock'){
+        timesWon += 1
+        winners = 'You win with rock smashing scissors'
+    }
+    
+    resultDisplay.innerHTML = `Overall Result: ${winners}`
+}
+
+function percentWon (){
+percentWinning = (timesWon/timesPlayed).toFixed(2)*100
+percentWonDisplay.innerHTML = `Luckiness today: ${percentWinning}%`
+}
+function Trophy(){
+//     if(winners === 'Sorry but you lose'){
+//         let winningImage = document.getElementById('winner')
+//         winningImage.src = "./assets/donald-trump.png"
+//         console.log('loser')
+//     }
+//     else if(winners === 'You win with scissors cutting paper'||'You win with paper covering rock'||'You win with rock smashing scissors'){
+//     let winningImage = document.getElementById('winner')
+//     winningImage.src = "./assets/crownWinner.png"
+//     console.log('winner')
+//    }
+    if(winners === 'draw'){
+    let winningImage = document.getElementById('winner')
+    winningImage.src = ""
+    console.log('draw')   
+    }
+    else if(winners === 'Sorry but you lose'){
+    let winningImage = document.getElementById('winner')
+    winningImage.src = "./assets/donald-trump.png"
+    console.log('loser')    
+    }
+    else{
+    let winningImage = document.getElementById('winner')
+    winningImage.src = "./assets/crownWinner.png"
+    console.log('winner') 
+    }
+}
+
